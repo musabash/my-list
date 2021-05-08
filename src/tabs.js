932b98cd3cbbc1b93386a6d77953cd023d095c02
musabash <basmusa@icloud.com>
2021-05-08 15:43:04 +0100

    Slider component was refactored

--- src/tabs.js
+++ src/tabs.js
@@ -5,16 +5,20 @@ export default function Tabs () {
    const {setActiveTab, activeTab} = useContext(AllContext)
    const Tab = ({value, name}) => <button className="tab" onClick={() => setActiveTab(value)}>{name}</button>
   return (
-    <div className="tabs">
-      <Tab value={true} name="completed" />
-      <Tab value={null} name="all" />
-      <Tab value={false} name="incomplete" />
-      <div 
+    <>
+      <div className="tabs">
+        <Tab value={true} name="completed" />
+        <Tab value={null} name="all" />
+        <Tab value={false} name="incomplete" />
+
+      </div>
+      <div className="slider-container">
+        <div 
         className="slider" 
         style={{transform: `translateX(${activeTab === null ? 0 : activeTab ? -158 : 158}%)`}}
-      >
+        >
+        </div>
       </div>
-    </div>
-      
+    </>
   )
 }
